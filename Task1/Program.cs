f19b21f6ecd22605db1d45d1935efa023647232c
Sure <schurovdaniil@gmail.com>
2024-02-05 19:29:09 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿string GetNum ()
+{
+    Console.WriteLine("Введите целое число");
+    string input = Console.ReadLine();
+    return input;
+}
+
+bool analyse(string input)
+{    
+    if (input == "q")
+    {
+        return true;
+    }
+    int count = 0;
+    int num = Convert.ToInt32(input);
+    while (num != 0)
+    {
+        count = count + num % 10;
+        num = num / 10;
+    }
+    if (count % 2 == 0)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}   
+
+
+while (analyse(GetNum()) == false)
+{
+    analyse(GetNum());
+} 
+Console.WriteLine("Спасибо что пользовались нашим ПО");
